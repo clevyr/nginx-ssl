664cfbec58a8bbc99718dc3d2a94f0b9958b8fea
Jacob McSwain <jacob@clevyr.com>
2019-04-12 15:47:25 -0500

    Add basic auth support with AUTH=true and USERNAME and PASSWORD

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,13 @@ FROM nginx:stable
 
 ENV URL localhost
 ENV EMAIL test@test.com
+ENV AUTH false
+ENV USERNAME username
+ENV PASSWORD password
 
 RUN apt-get update \
     && apt-get upgrade \
-    && apt-get install -y software-properties-common gpg \
+    && apt-get install -y software-properties-common gpg curl \
     && add-apt-repository -y ppa:certbot/certbot \
     && apt-get install -y python-certbot-nginx \
     && rm -rf /var/lib/apt/lists/*
--- startup.sh
+++ startup.sh
@@ -2,6 +2,26 @@ set -e
 
 cmd="$@"
 
+if [ $AUTH == "true" ]; then
+    if [ $PASSWORD == "password" ]; then
+        >&2 echo "Don't use a bad password, you know better"
+        exit
+    fi
+    SHA1PASS=$(echo -n ${PASSWORD} | openssl sha1 -r | head -c 40 | awk '{ print toupper($0) }')
+    KANON=$(echo -n ${SHA1PASS} | head -c 5 | awk '{ print toupper($0) }')
+    HASHES=$(curl https://api.pwnedpasswords.com/range/${KANON})
+    while read -r line; do
+        SUM=$(echo -n "$line" | head -c 35)
+        if [ $SHA1PASS == "${KANON}${SUM}" ]; then
+            >&2 echo "This password has been compromised before"
+            exit
+        fi
+    done <<< "$HASHES"
+    echo username:$(openssl passwd -apr1 ${PASSWORD}) >> /etc/nginx/.htpasswd
+    sed -i '/location/a auth_basic "Restricted Content";\nauth_basic_user_file /etc/nginx/.htpasswd;' /etc/nginx/conf.d/default.conf
+    >&2 echo "Authentication is set"
+fi
+
 domainString=""
 IFS=';' read -r -a domains <<< "$URL"
 for domain in "${domains[@]}"
@@ -13,5 +33,7 @@ done
 certbot --nginx -m $EMAIL $domainString --agree-tos -n --redirect
 killall nginx
 
->&2 echo "SSL Certs are created - executing command"
+>&2 echo "SSL Certs are created"
+
+>&2 echo "Starting nginx"
 nginx -g "daemon off;"
